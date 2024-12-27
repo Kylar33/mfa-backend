578e210cd836076645ed55877282f089b6222777
Kylar <gabii25.villa.gvm@gmail.com>
2024-12-27 16:35:38 +0100

    postgress

--- config/database.py
+++ config/database.py
@@ -6,12 +6,18 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+DATABASE_URL = os.environ.get('DATABASE_URL')  # Asegúrate de que DATABASE_URL está configurado en las variables de entorno.
 
+# Crear el motor de SQLAlchemy
 engine = create_engine(DATABASE_URL)
+
+# Configurar la sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declarative base para modelos
 Base = declarative_base()
 
+# Dependencia de la base de datos
 def get_db():
     db = SessionLocal()
     try:
